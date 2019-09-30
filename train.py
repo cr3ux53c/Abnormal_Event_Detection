6828e26f3a10a0c1f1596a15375faadc3b318d51
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-09-30 09:54:53 +0900

    포매팅

--- train.py
+++ train.py
@@ -15,48 +15,40 @@ Author: Harsh Tiku
 
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from model import load_model
-import numpy as np 
+import numpy as np
 import argparse
 
+# Running arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('n_epochs', type=int)
 
+args = parser.parse_args()
 
-parser=argparse.ArgumentParser()
-parser.add_argument('n_epochs',type=int)
+X_train = np.load('training.npy')
+frames = X_train.shape[2]  # X x Y x batch
 
-args=parser.parse_args()
+# Need to make number of frames divisible by 10
+frames = frames - frames % 10
+X_train = X_train[:, :, :frames]
+X_train = X_train.reshape(-1, 227, 227, 10)  # 10씩 끊어서
+X_train = np.expand_dims(X_train, axis=4)
+Y_train = X_train.copy()
 
-X_train=np.load('training.npy')
-frames=X_train.shape[2]
-#Need to make number of frames divisible by 10
+epochs = args.n_epochs
+batch_size = 1
 
+if __name__ == "__main__":
+    model = load_model()
 
-frames=frames-frames%10
+    callback_save = ModelCheckpoint("AnomalyDetector.h5",
+                                    monitor="mean_squared_error", save_best_only=True)
 
-X_train=X_train[:,:,:frames]
-X_train=X_train.reshape(-1,227,227,10)
-X_train=np.expand_dims(X_train,axis=4)
-Y_train=X_train.copy()
+    callback_early_stopping = EarlyStopping(monitor='val_loss', patience=3)
 
+    print('Model has been loaded')
 
-
-epochs=args.n_epochs
-batch_size=1
-
-
-
-if __name__=="__main__":
-
-	model=load_model()
-
-	callback_save = ModelCheckpoint("AnomalyDetector.h5",
-									monitor="mean_squared_error", save_best_only=True)
-
-	callback_early_stopping = EarlyStopping(monitor='val_loss', patience=3)
-
-	print('Model has been loaded')
-
-	model.fit(X_train,Y_train,
-			  batch_size=batch_size,
-			  epochs=epochs,
-			  callbacks = [callback_save,callback_early_stopping]
-			  )
+    model.fit(X_train, Y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              callbacks=[callback_save, callback_early_stopping]
+              )

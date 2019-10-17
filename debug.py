fab1e4ebe423e6796a81667d152ef7600dd6dd2a
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-10-17 11:04:29 +0900

    GPU 지원

--- debug.py
+++ debug.py
@@ -1,4 +1,12 @@
+import tensorflow as tf
 from tensorflow.python.client import device_lib
+
 print(device_lib.list_local_devices())
 
-
+# print('============')
+# print(tf.test.gpu_device_name())
+# print('============')
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# for gpu in gpus:
+#     print("Name:", gpu.name, "  Type:", gpu.device_type)
+# print(tf.__version__())

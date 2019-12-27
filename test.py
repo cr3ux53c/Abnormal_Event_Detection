ec9a7e984d36f196403b2d3e45e484e1f22e8ef3
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-12-27 10:25:28 +0900

    Print Result graph, Comment Overlapped vectors

--- test.py
+++ test.py
@@ -1,26 +1,25 @@
 """
-
 Testing module to test the presence of Anomalous Events in a Video
 
 The module computes reconstruction loss between input bunch and
-
 the reconstructed batch from the model, and flagges the batch as anomalous
 if loss value is greater than a given threshold.
 
-
-
-
-
-
-
-
 Author: Harsh Tiku
-
 """
 
 from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler, minmax_scale
 import numpy as np
+from matplotlib import pyplot as plt
 import argparse
+import tensorflow as tf
+
+config = tf.ConfigProto()
+
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
+
+tf.keras.backend.set_session(tf.Session(config=config))
 
 # Parsing arguments
 parser = argparse.ArgumentParser()
@@ -49,7 +48,10 @@ Lower the Threshhold,higher the chances that a bunch of frames will be flagged a
 
 '''
 
-threshold = 0.00043
+# threshold = 0.0004 # 사람이 큰 것들이 나타났을 때 검출
+# threshold = 0.0003795
+# threshold = 0.00017
+threshold = 0.000179
 
 model = load_model(args.model)
 
@@ -63,22 +65,60 @@ flag = 0  # Overall video flag
 frames = frames - frames % 10
 
 X_test = X_test[:, :, :frames]
+
+# X_test_overlapped = np.empty((227, 227, (int(frames * 2))), float)
+#
+# for width in range(227):
+#     for height in range(227):
+#         for start_idx in range(0, frames - 9, 5):
+#             X_test_overlapped[width, height, start_idx * 2:start_idx * 2 + 10] = X_test[width, height, start_idx:start_idx + 10]
+#
+# X_test_overlapped = X_test_overlapped[:, :, :frames * 2]
+# X_test_overlapped = X_test_overlapped.reshape(-1, 227, 227, 10)  # 10씩 끊어서
+# X_test_overlapped = np.expand_dims(X_test_overlapped, axis=4)
+
 X_test = X_test.reshape(-1, 227, 227, 10)
 X_test = np.expand_dims(X_test, axis=4)
 
+reconstruction_errors = []
+predictions = np.empty(0)
+
 for number, bunch in enumerate(X_test):
+    """
+    bunch <class 'tuple'>: (227, 227, 10, 1)
+    """
     n_bunch = np.expand_dims(bunch, axis=0)
     reconstructed_bunch = model.predict(n_bunch)
 
+    # Reconstruction Error
     loss = mean_squared_loss(n_bunch, reconstructed_bunch)
+    reconstruction_errors.append(loss)
 
     if loss > threshold:
-        print("Anomalous bunch of frames at bunch number {}".format(number))
+        print('Anomalous frames at bunch number %3d with loss: %.10f' % (number + 1, loss))
         flag = 1
-
-
+        predictions = np.append(predictions, 1)
     else:
-        print('Bunch Normal')
+        print('Normal frames at bunch number %3d with loss: %.10f' % (number + 1, loss))
+        predictions = np.append(predictions, 0)
 
 if flag == 1:
     print("Anomalous Events detected")
+
+# Evaluate Regularity score
+reconstruction_errors = np.asarray(reconstruction_errors)
+abnormal_score = minmax_scale(reconstruction_errors)
+regularity_score = 1 - abnormal_score
+# regularity_score = np.clip(regularity_score, 0, 1)
+plt.figure(figsize=(20, 4))
+plt.xticks(np.arange(0, 1600, 100.0))
+plt.plot(regularity_score)
+plt.title('Regularity Error (Threshold: %f)' % threshold)
+plt.show()
+
+plt.clf()
+plt.figure(figsize=(20, 4))
+plt.xticks(np.arange(0, 1600, 100.0))
+plt.plot(predictions)
+plt.title('Predictions (Threshold: %f)' % threshold)
+plt.show()
--- train.py
+++ train.py
@@ -12,7 +12,14 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 from model import load_model
 import numpy as np
 import argparse
+import tensorflow as tf
 
+
+config = tf.ConfigProto()
+
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
+
+tf.keras.backend.set_session(tf.Session(config=config))
 # Running arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('n_epochs', type=int)
@@ -27,9 +34,24 @@ frames = X_train.shape[2]  # X x Y x batch
 # Need to make number of frames divisible by 10
 frames = frames - frames % 10
 X_train = X_train[:, :, :frames]
+
+
+# X_train_overlapped = np.empty((227, 227, (int(frames*2))), float)
+#
+# for width in range(227):
+#     for height in range(227):
+#         for start_idx in range(0, frames - 9, 5):
+#             X_train_overlapped[width, height, start_idx*2:start_idx*2+10] = X_train[width, height, start_idx:start_idx + 10]
+#
+# X_train_overlapped = X_train_overlapped[:, :, :frames*2]
+# X_train_overlapped = X_train_overlapped.reshape(-1, 227, 227, 10)  # 10씩 끊어서
+# X_train_overlapped = np.expand_dims(X_train_overlapped, axis=4)
+# Y_train = X_train_overlapped.copy()
+
 X_train = X_train.reshape(-1, 227, 227, 10)  # 10씩 끊어서
 X_train = np.expand_dims(X_train, axis=4)
 Y_train = X_train.copy()
+
 
 epochs = args.n_epochs
 batch_size = 1

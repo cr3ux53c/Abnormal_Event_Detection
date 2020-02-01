c054fe1e1ed1addedb2776c8c55ad08bed04e58c
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-02-01 21:17:51 +0900

    add visualization.py

---
+++ visualization.py
@@ -0,0 +1,109 @@
+import numpy as np
+import argparse
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import minmax_scale
+
+# Parsing arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('gt_by_all_video_flatten', type=str)
+parser.add_argument('reconstruction_errors', type=str)
+args = parser.parse_args()
+
+# Set environment variables
+flag = 0  # Overall video flag
+# Define threshold for Sensitivity: Lower the Threshold, higher the chances that a bunch of frames will be flagged as Anomalous.
+# threshold = 0.0004 # 사람이 큰 것들이 나타났을 때 검출
+# threshold = 0.0003795
+# threshold = 0.00017
+threshold = 0.000179
+threshold_reconstruct1d = 0.7
+predictions = np.empty(0)
+predictions_reconstruct1d = np.empty(0)
+
+
+def IncreaseByTenfold(data):
+    expanded_data = np.empty(0)
+    for index, value in enumerate(data):
+        expanded_data = np.append(expanded_data, value)
+        for i in range(9):
+            expanded_data = np.append(expanded_data, value)
+    return expanded_data
+
+
+# Paint Ground Truth
+gt_by_all_video_flatten = np.load(args.gt_by_all_video_flatten)
+
+plt.figure(figsize=(80, 5))
+plt.title('Abnormal detection from Regularity score by threshold (Threshold: %f)' % 0.000179)
+plt.xticks(np.arange(0, gt_by_all_video_flatten.shape[0], step=50), rotation=45)
+for i, value in enumerate(gt_by_all_video_flatten):
+    if value == 1:
+        plt.axvspan(i, i, alpha=0.7, color='red')
+# Draw threshold
+plt.hlines(threshold, colors='black')
+
+# Evaluate reconstruction errors with threshold
+reconstruction_errors = np.load(args.reconstruction_errors)
+for number, reconstruction_error in enumerate(reconstruction_errors):
+    if reconstruction_error > threshold:
+        print('Anomalous frames at bunch number %3d with loss: %.10f' % (number + 1, reconstruction_error))
+        flag = 1
+        predictions = np.append(predictions, 1)
+    else:
+        print('Normal frames at bunch number %3d with loss: %.10f' % (number + 1, reconstruction_error))
+        predictions = np.append(predictions, 0)
+if flag == 1:
+    print("Anomalous Events detected")
+
+# Evaluate regularity-score (paper p.09)
+abnormal_score = minmax_scale(reconstruction_errors)
+regularity_score = 1 - abnormal_score
+regularity_score = np.clip(regularity_score, 0, 1)
+
+# Expand data
+expanded_regularity_score = IncreaseByTenfold(regularity_score)
+expanded_predictions = IncreaseByTenfold(predictions)
+
+# Draw regularity-score with predictions
+plt.plot(expanded_regularity_score, color='blue')
+for i, value in enumerate(expanded_predictions):
+    if value == 1:
+        plt.axvspan(i, i, alpha=0.3, color='orange')
+
+plt.show()
+plt.clf()
+
+# Reconstruct1D data
+plt.figure(figsize=(80, 5))
+plt.title('Abnormal detection from Reconstruct1D score by threshold (Threshold: %f)' % 0.000179)
+plt.xticks(np.arange(0, gt_by_all_video_flatten.shape[0], step=50), rotation=45)
+for i, value in enumerate(gt_by_all_video_flatten):
+    plt.plot(threshold, color='black')  # Draw threshold
+    if value == 1:
+        plt.axvspan(i, i, alpha=0.7, color='red')
+# Draw threshold
+plt.hlines(threshold_reconstruct1d, colors='black')
+
+# Evaluate Reconstruct1D errors with threshold
+reconstruct1d_errors = np.load('reconstruct1d_errors.npy')
+for number, reconstruct1d_error in enumerate(reconstruct1d_errors):
+    if reconstruct1d_error < threshold_reconstruct1d:
+        print('Anomalous frames at bunch number %3d with loss: %.10f' % (number + 1, reconstruct1d_error))
+        flag = 1
+        predictions_reconstruct1d = np.append(predictions_reconstruct1d, 1)
+    else:
+        print('Normal frames at bunch number %3d with loss: %.10f' % (number + 1, reconstruct1d_error))
+        predictions_reconstruct1d = np.append(predictions_reconstruct1d, 0)
+if flag == 1:
+    print("Anomalous Events detected")
+
+# Expand data
+expanded_reconstrct1d_score = IncreaseByTenfold(reconstruct1d_errors)
+expanded_predictions_reconstruct1d = IncreaseByTenfold(predictions_reconstruct1d)
+plt.plot(expanded_regularity_score, color='blue')
+plt.plot(expanded_reconstrct1d_score, color='magenta')
+for i, value in enumerate(expanded_predictions_reconstruct1d):
+    if value == 1:
+        plt.axvspan(i, i, alpha=0.3, color='orange')
+
+plt.show()
